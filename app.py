942e1d598d9659901198512af579250a69080b18
AGNIK321 <123392260+AGNIK321@users.noreply.github.com>
2026-04-01 22:45:49 +0530

    Remove installation command from app.py
    Removed pip install command for dependencies.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1siD89u-TK0iofAXTSqoDzaj-rY-eXYED
 """
 
-!pip install streamlit pandas numpy joblib plotly requests
+
 import streamlit as st
 import requests
 import pandas as pd
